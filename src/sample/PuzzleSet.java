52afd544d7c567d0d435a9d6dc541351f8df80c6
Joshua Seguin <JoshS.git@outlook.com>
2018-10-23 14:39:31 -0400

    Import PuzzleSet settings

--- src/sample/PuzzleSet.java
+++ src/sample/PuzzleSet.java
@@ -1,5 +1,12 @@
 package sample;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.*;
 public class PuzzleSet {
     private String name;
@@ -11,9 +18,28 @@ public class PuzzleSet {
         this.name = name;
     }
 
-    public void importPuzzleSet(){
-        //this is where puzzle set xml file is read
-        //we could have a importPuzzle in Puzzle to parse individual puzzles
+    public void importPuzzleSet(File puzzleFile){
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(puzzleFile);
+
+        // PuzzleSet settings
+        // Name
+        // Need to iterate through first layer of children since getElementsByTagName would return inner Puzzle names
+        NodeList nodes = document.getChildNodes();
+        for (int i = 0; i < nodes.getLength(); i++){
+            if (nodes.item(i).getNodeName().equals("name")){
+                this.name = nodes.item(i).getTextContent();
+            }
+        }
+
+        // Random order setting
+        String order = document.getElementsByTagName("randomOrder").item(0).getTextContent();
+        this.randomOrder = order.equals("true");
+
+        // Sequential puzzle completion setting
+        String seqComp = document.getElementsByTagName("sequentialComp").item(0).getTextContent();
+        this.sequentialCompletion = seqComp.equals("true");
     }
 
     public String getName() {
