f80013937e0499baaac93d8762f9f5f3b3e70ad8
Joshua Seguin <JoshS.git@outlook.com>
2018-10-23 14:54:05 -0400

    Add exception catching to XML parsing

--- src/sample/PuzzleSet.java
+++ src/sample/PuzzleSet.java
@@ -3,10 +3,13 @@ package sample;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 public class PuzzleSet {
     private String name;
@@ -20,8 +23,20 @@ public class PuzzleSet {
 
     public void importPuzzleSet(File puzzleFile){
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(puzzleFile);
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document document = null;
+        try {
+            document = documentBuilder.parse(puzzleFile);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // PuzzleSet settings
         // Name
