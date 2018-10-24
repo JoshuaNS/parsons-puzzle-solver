7af22148675da1a162875a85efdb6e5d1010fef5
Joshua Seguin <JoshS.git@outlook.com>
2018-10-24 14:07:32 -0400

    Simple check for correct solution

--- src/sample/DragNDropPuzzle.java
+++ src/sample/DragNDropPuzzle.java
@@ -2,6 +2,8 @@ package sample;
 
 import org.w3c.dom.Element;
 
+import java.util.ArrayList;
+
 public class DragNDropPuzzle extends Puzzle {
 
     public DragNDropPuzzle(Element puzzleXML) {
@@ -10,6 +12,20 @@ public class DragNDropPuzzle extends Puzzle {
 
     @Override
     Object checkSolution(Object providedSolution) {
-        return null;
+        if (!(providedSolution instanceof ArrayList)) {
+            System.err.println("Solution wasn't provided as ArrayList");
+            return null;
+        }
+        ArrayList<String> givenSolution = (ArrayList<String>)providedSolution;
+
+        if (getSolutions().size() != givenSolution.size()) {
+            return false;
+        }
+        for (int i = 0; i < getSolutions().size(); i++) {
+            if (!givenSolution.get(i).equals(getSolutions().get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
