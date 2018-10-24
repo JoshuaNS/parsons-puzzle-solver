05968977f3454b81e24e9cdf39bcca6b07a644fa
Iain Davidson <iain_davidson_@hotmail.com>
2018-10-24 17:07:12 -0400

    Add tests/error handling

--- test/sample/PuzzleSetTest.java
+++ test/sample/PuzzleSetTest.java
@@ -1,7 +1,40 @@
 package sample;
 
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PuzzleSetTest {
+    @Test
+    void importSamplePuzzle1() {
+        PuzzleSet ps = new PuzzleSet("Lab 1");
+        File f = new File("testfiles/puzzlesamp.xml");
+        ps.importPuzzleSet(f);
+        ArrayList<Puzzle> puzzles = ps.getPuzzles();
+        assertNotNull(puzzles.get(0));
+        assertEquals(puzzles.get(0).getName(), "Lab 1 - Puzzle 1");
+        assertNotNull(puzzles.get(1));
+        assertEquals(puzzles.get(1).getName(), "Lab 1 - Puzzle 2");
+    }
 
+    @Test
+    //test with Name, sequential, random all not specified
+    void importSamplePuzzleInvalid() {
+        PuzzleSet ps = new PuzzleSet();
+        File f = new File("testfiles/puzzlesamperror2.xml");
+        ps.importPuzzleSet(f);
+        assertEquals(ps.getName(), "Puzzle Set");
+        assertFalse(ps.isRandomOrder());
+        assertFalse(ps.isSequentialCompletion());
+    }
 }
--- test/sample/PuzzleTest.java
+++ test/sample/PuzzleTest.java
@@ -19,7 +19,7 @@ public class PuzzleTest {
 
     @Test
     void importSamplePuzzle1() {
-        File f = new File("puzzlesamp.xml");
+        File f = new File("testfiles/puzzlesamp.xml");
         assertTrue(f.exists());
 
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
@@ -68,5 +68,40 @@ public class PuzzleTest {
         assertLinesMatch(expectedSolution, p.getSolutions());
         assertNotNull(p.getDistractors());
         assertLinesMatch(expectedDistractors, p.getDistractors());
+    }
+    @Test
+    //uses an XML with no types, no indent, no name, no language, no description, and ensures it functions as desired
+    void importSamplePuzzleMissingParams() {
+        File f = new File("testfiles/puzzlesamperror1.xml");
+        assertTrue(f.exists());
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document document = null;
+        try {
+            document = documentBuilder.parse(f);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Puzzle p = new Puzzle((Element)document.getElementsByTagName("puzzle").item(0));
+        assertEquals(p.getType(), PuzzleType.DnD);
+        assertEquals(p.isIndentRequired(), false);
+        assertEquals(p.getName(), "Puzzle 1");
+        assertEquals(p.getLanguage(), "None Specified");
+        assertEquals(p.getDescription(), "");
+        Puzzle p2 = new Puzzle((Element)document.getElementsByTagName("puzzle").item(1));
+        assertEquals(p2.getType(), PuzzleType.MC);
+        assertEquals(p2.isIndentRequired(), false);
+        assertEquals(p2.getName(), "Puzzle 2");
+        assertEquals(p2.getLanguage(), "None Specified");
+        assertEquals(p2.getDescription(), "");
     }
 }
