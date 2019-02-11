b09df948fb7da5cd6f2d6465b3d1632a4f309cb9
TravisRidge <Travis.Ridge96@gmail.com>
2019-02-11 09:14:05 -0500

    Code clean-up, added distractor editor

--- src/parsonsolver/PuzzleCreatorController.java
+++ src/parsonsolver/PuzzleCreatorController.java
@@ -1,6 +1,5 @@
 package parsonsolver;
 
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.TextFieldListCell;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -23,9 +21,9 @@ import java.util.List;
 public class PuzzleCreatorController {
     private PuzzlePaneController rootController; //Link to the controller of the root for passing event
     private ObservableList<String> MCFalseAnswers = FXCollections.observableArrayList();
-    private int MCAnswerSelectedIndex = -1;
     private int nextLine = 1;
     private List<Integer> codeLines = new ArrayList<>();
+    private List<Integer> distractorLines = new ArrayList<>();
     private boolean shiftEnter = false;
 
     @FXML
@@ -35,7 +33,13 @@ public class PuzzleCreatorController {
     private TextArea SourceCodeBlocks;
 
     @FXML
+    private TextArea DistractorBlocks;
+
+    @FXML
     private TextArea SourceCodeEditor;
+
+    @FXML
+    private TextArea DistractorEditor;
 
     @FXML
     public void initialize() {
@@ -45,65 +49,14 @@ public class PuzzleCreatorController {
         codeLines = new ArrayList<>();
         codeLines.add(nextLine++);
 
-        SourceCodeEditor.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable,
-                                String oldValue, String newValue) {
-                if(shiftEnter){
-                    shiftEnter = false;
-                    return;
-                }
+        distractorLines = new ArrayList<>();
+        distractorLines.add(nextLine++);
 
-                if(oldValue != null && newValue != null && !oldValue.equals(newValue)) {
-                    int oldLength = oldValue.length();
-                    int newLength = newValue.length();
-                    int minLength = Math.min(oldLength, newLength); //Getter the smaller length
+        SourceCodeEditor.textProperty().addListener(this::SourceCodeChanged);
+        DistractorEditor.textProperty().addListener(this::DistractorChanged);
 
-                    int startChangeOffset;
-                    for (startChangeOffset = 0; startChangeOffset < minLength; startChangeOffset++) {
-                        if (oldValue.charAt(startChangeOffset) != newValue.charAt(startChangeOffset))
-                            break;
-                    }
-
-                    if(startChangeOffset == oldLength) {
-                        long lines = newValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
-                        for (int i = 0; i < lines; i++) {
-                            codeLines.add(nextLine++);
-                        }
-                    }
-                    else if(startChangeOffset == newLength) {
-                        long lines = oldValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
-                        for (int i = 0; i < lines; i++) {
-                            codeLines.remove(codeLines.size() - 1);
-                        }
-                    }
-                    else {
-                        int endChangeOffset;
-                        for (endChangeOffset = 1; startChangeOffset + endChangeOffset < minLength + 1; endChangeOffset++) {
-                            if (oldValue.charAt(oldLength - endChangeOffset) != newValue.charAt(newLength - endChangeOffset))
-                                break;
-                        }
-                        endChangeOffset--;
-
-                        long startLine = newValue.substring(0, startChangeOffset).chars().filter(ch -> ch == '\n').count();
-                        long addedLines = newValue.substring(startChangeOffset, newLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
-                        long removedLines = oldValue.substring(startChangeOffset, oldLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
-
-                        for (int i = 0; i < removedLines; i++) {
-                            codeLines.remove((int) startLine + 1);
-                        }
-
-                        for (int i = 0; i < addedLines; i++) {
-                            codeLines.add((int) startLine + i, nextLine++);
-                        }
-                    }
-
-                    printLineNums();
-                }
-            }
-        });
-
-        printLineNums();
+        SetSourceCodeLines();
+        SetDistractorLines();
     }
 
     /**
@@ -113,6 +66,128 @@ public class PuzzleCreatorController {
      */
     public void setRootController(PuzzlePaneController controller) {
         rootController = controller;
+    }
+
+    /**
+     * Handle changes to the source code text in the editor
+     * @param observable The {@code ObservableValue} which value changed
+     * @param oldValue The old value
+     * @param newValue The new value
+     */
+    public void SourceCodeChanged(ObservableValue<? extends String> observable,
+                                  String oldValue, String newValue){
+        if(shiftEnter){ //Shift-enter change propagates to here
+            shiftEnter = false;
+            return;
+        }
+
+        if(oldValue != null && newValue != null && !oldValue.equals(newValue)) {
+            int oldLength = oldValue.length();
+            int newLength = newValue.length();
+            int minLength = Math.min(oldLength, newLength); //Getter the smaller length
+
+            int startChangeOffset;
+            for (startChangeOffset = 0; startChangeOffset < minLength; startChangeOffset++) {
+                if (oldValue.charAt(startChangeOffset) != newValue.charAt(startChangeOffset))
+                    break;
+            }
+
+            if(startChangeOffset == oldLength) {
+                long lines = newValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                for (int i = 0; i < lines; i++) {
+                    codeLines.add(nextLine++);
+                }
+            }
+            else if(startChangeOffset == newLength) {
+                long lines = oldValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                for (int i = 0; i < lines; i++) {
+                    codeLines.remove(codeLines.size() - 1);
+                }
+            }
+            else {
+                int endChangeOffset;
+                for (endChangeOffset = 1; startChangeOffset + endChangeOffset < minLength + 1; endChangeOffset++) {
+                    if (oldValue.charAt(oldLength - endChangeOffset) != newValue.charAt(newLength - endChangeOffset))
+                        break;
+                }
+                endChangeOffset--;
+
+                long startLine = newValue.substring(0, startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                long addedLines = newValue.substring(startChangeOffset, newLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
+                long removedLines = oldValue.substring(startChangeOffset, oldLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
+
+                for (int i = 0; i < removedLines; i++) {
+                    codeLines.remove((int) startLine + 1);
+                }
+
+                for (int i = 0; i < addedLines; i++) {
+                    codeLines.add((int) startLine + i, nextLine++);
+                }
+            }
+
+            SetSourceCodeLines();
+        }
+    }
+
+    /**
+     * Handle changes to the distractor text in the editor
+     * @param observable The {@code ObservableValue} which value changed
+     * @param oldValue The old value
+     * @param newValue The new value
+     */
+    public void DistractorChanged(ObservableValue<? extends String> observable,
+                                  String oldValue, String newValue){
+        if(shiftEnter){ //Shift-enter change propagates to here
+            shiftEnter = false;
+            return;
+        }
+
+        if(oldValue != null && newValue != null && !oldValue.equals(newValue)) {
+            int oldLength = oldValue.length();
+            int newLength = newValue.length();
+            int minLength = Math.min(oldLength, newLength); //Getter the smaller length
+
+            int startChangeOffset;
+            for (startChangeOffset = 0; startChangeOffset < minLength; startChangeOffset++) {
+                if (oldValue.charAt(startChangeOffset) != newValue.charAt(startChangeOffset))
+                    break;
+            }
+
+            if(startChangeOffset == oldLength) {
+                long lines = newValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                for (int i = 0; i < lines; i++) {
+                    distractorLines.add(nextLine++);
+                }
+            }
+            else if(startChangeOffset == newLength) {
+                long lines = oldValue.substring(startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                for (int i = 0; i < lines; i++) {
+                    distractorLines.remove(distractorLines.size() - 1);
+                }
+            }
+            else {
+                int endChangeOffset;
+                for (endChangeOffset = 1; startChangeOffset + endChangeOffset < minLength + 1; endChangeOffset++) {
+                    if (oldValue.charAt(oldLength - endChangeOffset) != newValue.charAt(newLength - endChangeOffset))
+                        break;
+                }
+                endChangeOffset--;
+
+                long startLine = newValue.substring(0, startChangeOffset).chars().filter(ch -> ch == '\n').count();
+                long addedLines = newValue.substring(startChangeOffset, newLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
+                long removedLines = oldValue.substring(startChangeOffset, oldLength - endChangeOffset).chars().filter(ch -> ch == '\n').count();
+
+                for (int i = 0; i < removedLines; i++) {
+                    distractorLines.remove((int) startLine + 1);
+                }
+
+                for (int i = 0; i < addedLines; i++) {
+                    distractorLines.add((int) startLine + i, nextLine++);
+                }
+            }
+
+            SetDistractorLines();
+        }
     }
 
     /**
@@ -132,10 +207,35 @@ public class PuzzleCreatorController {
             SourceCodeEditor.setText(newText);
             SourceCodeEditor.positionCaret(index+1);
 
-            printLineNums();
+            SetSourceCodeLines();
         }
     }
 
+    /**
+     * Get key events in the distractor editor, and sets flags as needed
+     * @param event
+     */
+    @FXML
+    public void DistractorKeyEvent(KeyEvent event) {
+        if(event.getEventType() == KeyEvent.KEY_RELEASED && event.getCode() == KeyCode.ENTER && event.isShiftDown()){
+            int index = DistractorEditor.getCaretPosition();
+            shiftEnter = true;
+
+            long startLine = DistractorEditor.getText(0,index).chars().filter(ch -> ch == '\n').count();
+            distractorLines.add((int) startLine + 1, distractorLines.get((int)startLine));
+
+            String newText = DistractorEditor.getText(0,index) + "\n" + DistractorEditor.getText().substring(index);
+            DistractorEditor.setText(newText);
+            DistractorEditor.positionCaret(index+1);
+
+            SetDistractorLines();
+        }
+    }
+
+    /**
+     * Commit changes to the MC Answer
+     * @param event EditEvent from the ListView
+     */
     @FXML
     public void CommitMCAnswerEdit(ListView.EditEvent<String> event){
         //Add validation of new value here
@@ -147,46 +247,58 @@ public class PuzzleCreatorController {
 
     /**
      * Debugging event for editing of the MC Answers list
-     * @param event
+     * @param event EditEvent from the ListView
      */
     @FXML
     public void MCAnswerEdit(ListView.EditEvent<String> event){
         System.out.println(event.getEventType() + "\t" + event.getIndex() + "\t" + event.getNewValue());
     }
 
+    /**
+     * Add a new MC Answer to the list
+     * @param event ActionEvent of button press
+     */
     @FXML
     public void AddMCAnswer(ActionEvent event) {
         MCAnswersList.getItems().add("New Answer");
     }
 
+    /**
+     * Remove a MC answer from the list
+     * @param event ActionEvent of button press
+     */
     @FXML
     public void RemoveMCAnswer(ActionEvent event) {
         MCAnswersList.getItems().remove(MCAnswersList.getItems().size() - 1);
     }
 
-    @FXML
-    public void KeyReleased(KeyEvent event) {
-        if(event.getCode() == KeyCode.ENTER && MCAnswerSelectedIndex != -1){
-            MCAnswersList.getItems().add(MCAnswerSelectedIndex + 1,"New Answer");
-        }
-    }
-
-    @FXML
-    public void InputMethodChanged(InputMethodEvent event){
-        System.out.println(event.getEventType());
-    }
-
-    private void printLineNums(){
+    /**
+     * Update the texts of the blocks list
+     */
+    private void SetSourceCodeLines(){
         int lineNum = 1;
         String codeColumns = "" + lineNum++;
         for(int i = 1; i < codeLines.size(); i++) {
-            if (codeLines.get(i) == codeLines.get(i - 1)) {
-                codeColumns += "\n-";
-            }
-            else {
-                codeColumns += "\n" + lineNum++;
-            }
+            if (codeLines.get(i).equals(codeLines.get(i - 1)))
+                codeColumns = codeColumns.concat("\n-");
+            else
+                codeColumns = codeColumns.concat("\n" + lineNum++);
         }
         SourceCodeBlocks.setText(codeColumns);
+    }
+
+    /**
+     * Update the texts of the blocks list
+     */
+    private void SetDistractorLines(){
+        int lineNum = 1;
+        String codeColumns = "X" + lineNum++;
+        for(int i = 1; i < distractorLines.size(); i++) {
+            if (distractorLines.get(i).equals(distractorLines.get(i - 1)))
+                codeColumns = codeColumns.concat("\n-");
+            else
+                codeColumns = codeColumns.concat("\nX" + lineNum++);
+        }
+        DistractorBlocks.setText(codeColumns);
     }
 }
